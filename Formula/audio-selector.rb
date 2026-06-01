fe758d54f9603d1cb10e43ec021d5ed7083ed533
matsuokashuhei <matsuokashuheiii@gmail.com>
2026-06-01 23:16:49 +0900

    Update audio-selector formula to 0.1.6

--- Formula/audio-selector.rb
+++ Formula/audio-selector.rb
@@ -1,8 +1,8 @@
 class AudioSelector < Formula
   desc "Keyboard-driven macOS audio input and output selector"
   homepage "https://github.com/matsuokashuhei/audio-selector"
-  url "https://github.com/matsuokashuhei/audio-selector/archive/refs/tags/v0.1.5.tar.gz"
-  sha256 "af9faf5f24ee5ea033444227501b36414e632a5d5a031da6f4ce5ba966fa68a6"
+  url "https://github.com/matsuokashuhei/audio-selector/archive/refs/tags/v0.1.6.tar.gz"
+  sha256 "185c05effb62bd5a45c5bf311f15e2cfaa08101a6a55d80051be2a8c78ccb041"
   license "MIT"
 
   depends_on xcode: ["16.0", :build]
